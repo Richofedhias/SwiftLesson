28eef4d14fe4798eb4f4363d512ac34ba57df992
richofedhias <fedhia64@gmail.com>
2023-11-04 16:05:28 +0700

    Initial Commit

---
+++ SwiftLesson/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftLesson
+//
+//  Created by Richo Fedhia on 04/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftLesson/SwiftLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLessonApp.swift
+//  SwiftLesson
+//
+//  Created by Richo Fedhia on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
